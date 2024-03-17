2313deb00731b66a930c672a02d146f2e440608e
NikkiSansom <dsansom534@gmail.com>
2024-03-17 16:06:24 +0000

    Issue-1: Added in while loops to allow the user to input wrong options without manually restarting the program.

--- finance_calculators.py
+++ finance_calculators.py
@@ -13,44 +13,48 @@ print("investment - to calculate the amount of interest you'll earn on your inve
 print("bond - to calculate the amount you'll have to pay on a home loan\n")
 
 # User selects either 'investment' or 'bond' (any capitalisation will work)
-calculator_use = input("Enter either 'investment' or 'bond' from the menu above to proceed: ")
-calculator_use = calculator_use.lower()
+while True:
+   calculator_use = input("Enter either 'investment' or 'bond' from the menu above to proceed: ")
+   calculator_use = calculator_use.lower()
 
 #### Investment Calculations ####
-if calculator_use == "investment":
-    money_deposited = float(input("How much money are you depositing (£)?: "))
-    interest_rate = float(input("What is the interest rate (%)? :"))
-    num_years = int(input("How many years are you investing for?: "))
+   if calculator_use == "investment":
+      money_deposited = float(input("How much money are you depositing (£)?: "))
+      interest_rate = float(input("What is the interest rate (%)? :"))
+      num_years = int(input("How many years are you investing for?: "))
 
-    interest = input("Simple or Compound Interest?: ")
-    interest = interest.lower()
-    # Simple Interest Calculation
-    if interest == "simple":
-       investment_returns = money_deposited*(1 + ((interest_rate/100)*num_years))   # A = P * (1 + r*t)
-       print("Investment Returns: " + "£" + str(round(investment_returns, 2)))
-       print()
-    # Compound Interest Calculation
-    elif interest == "compound":
-       investment_returns = money_deposited * math.pow((1+(interest_rate/100)), num_years)  # A = P * math.pow((1+r),t)
-       print("Investment Returns: " + "£" + str(round(investment_returns, 2)))
-       print()
-    # If anything but simple or compound are entered by the user:
-    else:
-       print("There was an error with your input.")
-       print("Please restart the Financial Calculator and try again.")
+      while True:
+         interest = input("Simple or Compound Interest?: ")
+         interest = interest.lower()
+         # Simple Interest Calculation
+         if interest == "simple":
+            investment_returns = money_deposited*(1 + ((interest_rate/100)*num_years))   # A = P * (1 + r*t)
+            print("Investment Returns: " + "£" + str(round(investment_returns, 2)))
+            print()
+            break
+                  # Compound Interest Calculation
+         elif interest == "compound":
+            investment_returns = money_deposited * math.pow((1+(interest_rate/100)), num_years)  # A = P * math.pow((1+r),t)
+            print("Investment Returns: " + "£" + str(round(investment_returns, 2)))
+            print()
+            break
+         # If anything but simple or compound are entered by the user:
+         else:
+            print("There was an error with your input, please try again.")
+            continue
+      break
 
 #### Bond Calculations ####
-elif calculator_use == "bond":
-    house_value = float(input("What is the present value of the house (£)?: "))
-    interest_rate = float(input("What is the annual interest rate (%)? :"))
-    repay_months = int(input("What is the number of months you plan to take to repay the bond?: "))
-    monthly_interest = (interest_rate/100)/12
-    repayment = (monthly_interest*house_value)/(1-((1 + monthly_interest)**(-1 * repay_months)))    # repayment = (i*P)/(1-((1+i)**(-n)))
-    print("Monthly Repayments: " + "£" + str(round(repayment, 2)))
-    print()
+   elif calculator_use == "bond":
+      house_value = float(input("What is the present value of the house (£)?: "))
+      interest_rate = float(input("What is the annual interest rate (%)? :"))
+      repay_months = int(input("What is the number of months you plan to take to repay the bond?: "))
+      monthly_interest = (interest_rate/100)/12
+      repayment = (monthly_interest*house_value)/(1-((1 + monthly_interest)**(-1 * repay_months)))    # repayment = (i*P)/(1-((1+i)**(-n)))
+      print("Monthly Repayments: " + "£" + str(round(repayment, 2)))
+      print()
+      break
 
-##### If anything but investment or bond are entered by the user ####
-else:
-    print("There was an error with your input.")
-    print("Please restart the Financial Calculator and try again.")
-    print()
+   else:
+      print("There was an error with your input, please try again.")
+      continue
